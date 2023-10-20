595c705b54236ae0b2a251dadea1f1b63f62acbf
Felix Arntz <flixos90@gmail.com>
2023-10-20 17:21:44 -0500

    Fix dumb bug.

--- wp-api-json-feed.php
+++ wp-api-json-feed.php
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wp_api_json_feed_load() {
 	load_plugin_textdomain( 'wp-api-json-feed' );
 
-	if ( version_compare( get_bloginfo( 'version' ), '5.4.0', '<' ) ) {
+	if ( version_compare( get_bloginfo( 'version' ), '5.4', '<' ) ) {
 		add_action( 'admin_notices', 'wp_api_json_feed_show_version_error_notice', 10, 0 );
 		return;
 	}
